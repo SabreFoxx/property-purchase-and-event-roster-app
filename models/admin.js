2c4ece00b7ea13817bff3d0c204dbc09f241fdc0
SabreFoxx <geraldnebs@gmail.com>
2021-09-14 21:52:34 +0100

    admin auth

--- models/admin.js
+++ models/admin.js
@@ -1,4 +1,8 @@
+import env from 'dotenv';
 import * as sequelize from 'sequelize';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+env.config();
 
 const { Model } = sequelize.default || sequelize;
 
@@ -12,7 +16,60 @@ export default (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+
+    /**
+     * Login a user using email and password
+     * @param {string} email email address
+     * @return {Promise}
+     */
+    static loginUsing(email, plainPassword) {
+      return Admin.findOne({
+        where: {
+          email: email
+        }
+      }).then(user => {
+        if (user == null)
+          return null;
+        if (user.isPasswordValid(plainPassword)) return user;
+        else return null;
+      }).catch(err => console.log(err));
+    };
+
+    /**
+     * creates a password hash, from the plain string
+     * @param {string} plainPassword plain password string
+     */
+    setPassword(plainPassword) {
+      this.setDataValue('passwordSalt', crypto.randomBytes(16).toString('hex'));
+      this.setDataValue('password', crypto
+        .pbkdf2Sync(plainPassword, this.passwordSalt, 1000, 64, 'sha512')
+        .toString('hex'));
+    }
+
+    /**
+     * is the plain password correct?
+     * @param {string} plainPassword plain password string
+     */
+    isPasswordValid(plainPassword) {
+      const passwordHash = crypto
+        .pbkdf2Sync(plainPassword, this.getDataValue('passwordSalt'), 1000, 64, 'sha512')
+        .toString('hex');
+      return this.getDataValue('password') === passwordHash;
+    }
+
+    /**
+     * generates a signed jwt token for this user account object
+     */
+    generateJwt() {
+      const expiry = new Date;
+      expiry.setDate(expiry.getDate() + 7);
+      return jwt.sign({
+        email: this.getDataValue('email'),
+        exp: parseInt(expiry.getTime() / 1000, 10)
+      }, process.env.JWT_SECRET);
+    }
   };
+
   Admin.init({
     email: DataTypes.STRING,
     password: DataTypes.STRING,
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,22 @@
 import express from 'express'
+import env from 'dotenv';
+import expressJwt from 'express-jwt';
 
 const router = express.Router();
+env.config();
+
+// authentication middleware that helps us decode our jwt token and saves it in express' req object
+const auth = expressJwt({
+    secret: process.env.JWT_SECRET,
+    // we'll put this property "payload" containing our decrypted jwt, inside express' req object
+    userProperty: 'payload',
+    algorithms: ['HS256']
+    // if we didn't supply a valid token, control flow continues at the bottom error handler in app.js
+});
+
+import { register, login } from '../controllers/auth.js';
+
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
