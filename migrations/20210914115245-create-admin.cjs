f60e055fde886db8a827dd06baa0887da157f26e
SabreFoxx <geraldnebs@gmail.com>
2021-09-18 23:25:37 +0100

    unique admin email

--- migrations/20210914115245-create-admin.cjs
+++ migrations/20210914115245-create-admin.cjs
@@ -17,7 +17,8 @@ module.exports = {
         allowNull: true
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       password: {
         type: Sequelize.STRING
