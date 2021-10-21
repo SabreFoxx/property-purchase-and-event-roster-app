227a06d206f903528409591d9bc81289209ef28d
SabreFoxx <geraldnebs@gmail.com>
2021-10-21 15:40:35 +0100

    send email to admin

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ import Sequelize from 'sequelize';
 import models, { sequelize } from '../models/index.js';
 import sha256 from 'js-sha256';
 import { confirmPayment } from './property.js';
+import { sendMail } from '../utilities/functions.js';
 
 const addCohost = async (req, res) => {
     // generates n pin initializers to be used in a Model.create() method
@@ -306,9 +307,9 @@ const submitPaymentDocument = (req, res) => {
         // TODO send document to admin's email
         const document = {
             name: req.body.name,
-            image: req.body.base64Document,
-            plotId: req.body.plotId
+            image: req.body.base64Document
         }
+        sendMail(req.body.referenceId, req.body.name);
         res.status(200)
             .json({ message: "Document submitted successfully" });
 
--- utilities/functions.js
+++ utilities/functions.js
@@ -1,3 +1,34 @@
+import nodemailer from 'nodemailer';
+import env from 'dotenv';
+env.config();
+
 export const uniqueId = () => {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
+
+export const sendMail = (referenceId, name) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: process.env.NODEMAILER_EMAIL,
+            pass: process.env.NODEMAILER_EMAIL_PASSWORD
+        },
+    });
+
+    // send mail with defined transport object
+    transporter.sendMail({
+        from: `"${req.body.name}" <bonitasmailer@gmail.com>`, // sender address
+        to: "gerald.nnebe@bonitasict.com, gerald.nnebe@bonitasict.com", // list of receivers
+        subject: `${Date.now()} GVE Groundbreaking app offline payment receipt/`, // Subject line
+        // text: `From ${req.body.name}: ${req.body.body}`, // plain text body
+        html: `<p><b>From: ${name}</b></p><p><b>Reference Id: ${referenceId}</b></p>`,
+    }).then(() => {
+        // console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        res.status(200).send('Message sent successfully');
+    }).catch(e => console.log(e));
+};
